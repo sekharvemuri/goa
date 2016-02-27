3a326f9d9cdba3c0c918c976ac29a9f071e575fa
rangaphanikumar <rangaphanikumar@gmail.com>
2016-02-28 01:17:02 +0530

    Pavan changes

--- src/main/java/com/guru/order/converter/CommodityConverter.java
+++ src/main/java/com/guru/order/converter/CommodityConverter.java
@@ -1,15 +1,18 @@
 package com.guru.order.converter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.guru.order.data.vo.CommodityVO;
 import com.guru.order.dto.CommodityDTO;
 import com.guru.order.utils.CollectionUtils;
+import com.guru.order.utils.DateUtils;
 
 public class CommodityConverter {
-	
-	public static List<CommodityDTO> getCommodities(List<CommodityVO> symbolsList) {
+
+	public static List<CommodityDTO> getCommodities(
+			List<CommodityVO> symbolsList) {
 		List<CommodityDTO> dtoList = new ArrayList<CommodityDTO>();
 		if (CollectionUtils.isNotEmpty(symbolsList)) {
 			for (CommodityVO vo : symbolsList) {
@@ -20,7 +23,16 @@ public class CommodityConverter {
 	}
 
 	private static CommodityDTO getCommodityDto(CommodityVO vo) {
-		return new CommodityDTO(vo.getId(), vo.getName());
+		CommodityDTO dto = new CommodityDTO(vo.getId(), vo.getName());
+		if (CollectionUtils.isNotEmpty(vo.getExpiryDates())) {
+			List<String> list = new ArrayList<String>(vo.getExpiryDates()
+					.size());
+			for (Date date : vo.getExpiryDates()) {
+				list.add(DateUtils.formatToDDMMMYY(date));
+			}
+			dto.setExpiryDates(list);
+		}
+		return dto;
 	}
 
 }
